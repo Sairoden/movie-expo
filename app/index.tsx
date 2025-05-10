29aca17fee534e0b2c3cbc9a64024f2faea3105a
Sairoden Gandarosa <odingandarosa@gmail.com>
2025-05-10 13:46:04 +0800

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-7xl text-accent">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
